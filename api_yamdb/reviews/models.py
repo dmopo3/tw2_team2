12ee0e5ae609fe9dcb8cdf85bfe380c34fcd1adf
Михаил Баранов <dmopo3@yandex.ru>
2022-03-31 13:59:42 +0300

    add models description

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Role(models.TextChoices):
+    """Права доступа для пользователей."""
+    pass
+
+
+class User(AbstractUser):
+    """Модель пользователя."""
+    pass
+
+
+class Categories(models.Model):
+    """Категории произведений (Фильмы, книги и тд)."""
+    pass
+
+
+class Genres(models.Model):
+    """Жанры произведений"""
+    pass
+
+
+class Titles(models.Model):
+    """Произведения"""
+    pass
+
+
+class Reviews(models.Model):
+    """Отзывы на произведения"""
+    pass
+
+
+class Comments(models.Model):
+    """Коментарии к отзывам"""
+    pass
