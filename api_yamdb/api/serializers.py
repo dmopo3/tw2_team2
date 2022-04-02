c87264b3286cc512db711459ea7c87e0869e853e
Ирина Фок <fokira@mail.ru>
2022-04-02 18:14:19 +0300

    Add ReviewsSerializer and CommentsSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from reviews.models import User
+from rest_framework.generics import get_object_or_404
+from reviews.models import Comments, Reviews, Titles, User
 
 
 class SendEmailSerializer(serializers.ModelSerializer):
@@ -18,3 +19,47 @@ class SendTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'confirmation_code')
+
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    """Класс для преобразования данных отзыва."""
+
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        many=False,
+    )
+
+    def validate(self, attr):
+        request = self.context['request']
+        if request.method != 'POST':
+            return attr
+        author = request.user
+        title_id = request.kwargs.get('title_id')
+        title = get_object_or_404(Titles, pk=title_id)
+        if Reviews.objects.filter(title=title, author=author).exists():
+            raise serializers.ValidationError(
+                'Вы уже оставили отзыв на данное произведение'
+            )
+        return attr
+
+    class Meta:
+        model = Reviews
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """Класс для преобразования данных комментария."""
+
+    review = serializers.SlugRelatedField(slug_field='text', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    class Meta:
+        model = Comments
+        fields = ('id', 'text', 'author', 'pub_date')
